76c2e6cfc6843a26e35c56983ac9d5a59c73b26c
A.J. Orians <ajorians@gmail.com>
2022-07-03 00:12:08 -0400

    Added a script that will start a slide show.
    Mean to run via Crontab.

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+TIME=$(date +%H%M)
+if [ $TIME -eq 2331 ]; then
+   echo "Killing pqiv to start fresh"
+   killall pqiv
+   sleep 10
+fi
+
+for pid in $(pidof -x pqiv); do
+    if [ $pid != $$ ]; then
+        echo "[$(date)] : pqiv : Process is already running with PID $pid"
+        exit 1
+    fi
+done
+
+pqiv --slideshow-interval=10 --slideshow --display=:0 --fullscreen --hide-info-box /mnt/photos/* &
+
+#qiv --display :0 --slide --delay 5 --fullscreen --scale_down --no_statusbar /mnt/photos/*
+
+#viewnior --slideshow --fullscreen --display=:0 /mnt/photos/*
+
+
